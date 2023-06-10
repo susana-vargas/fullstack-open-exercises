ed01120e16683781183512e5f009544990a5a4a5
Susana <hernandeznerisusanaisabel@gmail.com>
2023-06-10 12:06:03 -0600

    paso-2:completed

--- part1/courseinfo/anecdotas/src/App.js
+++ part1/courseinfo/anecdotas/src/App.js
@@ -7,8 +7,6 @@ const Button = (props) => (
 )
 
 const App = () => {
-  //se llama al hook useState para darle estado al componente
-  const [selected, setSelected] = useState(0);  
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -17,9 +15,13 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
+    'The only way to go fast, is to go well.',
   ];
-
+  //se llama al hook useState para darle estado al componente
+  const [selected, setSelected] = useState(0);  
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  
+  
   const randomNumber = (min, max) => {
     const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
     return randomNumber
@@ -29,13 +31,36 @@ const App = () => {
     const min = 0
     const max = anecdotes.length - 1
     const randomNum = randomNumber(min, max)    
+    console.log('indice', randomNum);
     setSelected(randomNum)
-  }  
+  } 
+  
+  console.log(votes)
+  
+  const vote = () => {
+    const copy = [...votes]
+    copy[selected] = copy[selected] + 1
+    setVotes(copy)    
+  }
+
+  let number1 = 0
+  const analisis = () => {
+    
+    number1 = number1 + 1
+    console.log(number1)
+    number1 += 1
+    console.log(number1)
+    number1++
+    console.log(number1)
+  }
 
   return (
     <div>
-      {anecdotes[selected]}
+      {anecdotes[selected]} <br />
+      <p>has {votes[selected]} votes</p>
       <Button handleClick={onClick}  text='next anecdote'/>
+      <Button handleClick={vote}  text='vote'/>
+      <Button handleClick={analisis}  text='analisis'/>
     </div>
   )
 }
