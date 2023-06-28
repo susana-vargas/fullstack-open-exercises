827f5d3d9677fa7d9be1b25d5450b071f3b20b57
Susana <hernandeznerisusanaisabel@gmail.com>
2023-06-28 16:23:24 -0600

    agenda-telefonica_paso4:commit para revicion

--- part2/agenda-telefonica/src/App.js
+++ part2/agenda-telefonica/src/App.js
@@ -2,7 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' , id: 1, number: 5577116284}
+    { name: 'Arto Hellas' , id: 1, number: 5577116284},
+    { name: 'Ada Lovelace' , id: 2, number: 39445323523},
+    { name: 'Dan Abramov' , id: 3, number: 1243234345},
+    { name: 'Mary Poppendieck' , id: 4, number: 39236423122}
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -20,7 +23,7 @@ const App = () => {
     event.preventDefault()
       //si no encuentra algo que cumpla la condicion el valer de la consatante sera indefinido
     const personFinded = persons.find((person) => person.name === newName);
-    //si la constante es una desigualdad estricta se ejecuta una  condicion, si esta repetida
+    //si la constante es una desigualdad estricta se ejecuta una condicion, si esta repetida
     //muestra el alert si no, se ejecuta el obgeto 
     if (personFinded !== undefined) {
       alert(newName + ' ya esta en la guía telefónica ' + newNumber)
@@ -34,12 +37,18 @@ const App = () => {
       //y se le concatena al arreglo 
       setNewName('')
       setNewNumber('')
+      
     }    
   }
-  
+  const filteredNames = persons.filter((person) => person.name.includes(newName))
+      console.log(filteredNames, 'desde filter e includes');
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with: <input value={newName} onChange={handleNameInputChange} />
+        </div>
+        <h2>add a new</h2>
       <form onSubmit={addRecord}>
         <div>
           name: <input value={newName} onChange={handleNameInputChange} />
