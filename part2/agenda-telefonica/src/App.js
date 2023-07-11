7a6defd54090fa1cd2ca4eeb590287dfc8b5b789
Susana <hernandeznerisusanaisabel@gmail.com>
2023-07-11 15:43:42 -0600

    segundaPart_ agenda-telefonica: effect-hooks :: completed

--- part2/agenda-telefonica/src/App.js
+++ part2/agenda-telefonica/src/App.js
@@ -1,34 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 import Filter from './components/Filter';
 import PersonList from './components/PersonList';
 import PersonForm from './components/PersonForm';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 1, name: 'Juan Martinez', number: '1234567890' },
-    { id: 2, name: 'María', number: '9876543210' },
-    { id: 3, name: 'Carlos', number: '5551234567' },
-    { id: 4, name: 'Laura', number: '9998887777' },
-    { id: 5, name: 'Pedro', number: '4445556666' },
-    { id: 6, name: 'Ana', number: '1112223333' },
-    { id: 7, name: 'Luis', number: '9990001111' },
-    { id: 8, name: 'Carmen', number: '7778889999' },
-    { id: 9, name: 'Javier', number: '3334445555' },
-    { id: 10, name: 'Sofía', number: '2223334444' },
-    { id: 11, name: 'Miguel', number: '6667778888' },
-    { id: 12, name: 'Isabella', number: '5556667777' },
-    { id: 13, name: 'Andrés', number: '8889990000' },
-    { id: 14, name: 'Valentina', number: '7770001111' },
-    { id: 15, name: 'Diego', number: '1112223333' },
-    { id: 16, name: 'Camila', number: '4445556666' },
-    { id: 17, name: 'José', number: '9998887777' },
-    { id: 18, name: 'Lucía', number: '5551234567' },
-    { id: 19, name: 'Fernando', number: '9876543210' },
-    { id: 20, name: 'Gabriela', number: '1234567890' },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+    
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  
 
   const handleNameInputChange = (event) => {
     setNewName(event.target.value);
